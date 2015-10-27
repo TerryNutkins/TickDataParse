9833da02157074a4ac03d4be6a21085279313f07
TerryNutkins <passport@noelwatson.com>
2015-10-27 10:51:55 +0000

    Intraday add

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TickDataParse
 {
@@ -10,7 +8,37 @@ namespace TickDataParse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is a test");
+
+            //RX here
+            //http://weareadaptive.com/blog/2015/07/16/historical-time-series-data-rx/
+            //APPL tick data
+            //https://sites.google.com/site/algorithmictradingbook/website-builder
+            //Pg40 of book contains description of data
+            //http://www.amazon.co.uk/Algorithmic-High-Frequency-Trading-Mathematics-Finance/dp/1107091144
+
+
+            
+
+            Dictionary<string, string> orders = new Dictionary<string, string>();
+            //Read all but first line
+            //https://msdn.microsoft.com/en-GB/library/ezwyzy7b.aspx
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Noel\CodingRepository\TickDataParse\AAPL20130730nasdaq.txt").Skip(1).ToArray();
+
+
+
+         
+
+            foreach (string line in lines)
+            {
+                string[] splitLines = line.Split(',');
+
+                if (splitLines.Length==1)
+                {
+                    Console.WriteLine("Ignoring line as it has only one value");
+                }
+
+            }
+
             Console.ReadLine();
 
 
