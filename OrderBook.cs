1d586bf8a159cdd3b9f5d8b85acaa1f5782e254b
TerryNutkins <passport@noelwatson.com>
2015-10-28 05:59:04 +0000

    Adding orderbook

---
+++ OrderBook.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TickDataParse
+{
+
+    enum Way
+    {
+        Buy,
+        Sell
+    }
+
+    class OrderBook
+    {
+        //Key on price with values being list of orders
+        SortedDictionary<int, List<Order>> orders = new SortedDictionary<int, List<Order>>();
+        Way orderway = new Way();
+
+        public OrderBook(Way way)
+        {
+            orderway = way;
+        }
+
+
+        void AddOrder(int orderID, int quantity, int Price)
+        {
+
+        }
+
+        void PartiallyCancelOrder(int orderID, int quantity, int Price)
+        {
+
+
+        }
+
+        void DeleteOrder(int orderID, int quantity, int Price)
+        {
+
+
+        }
+    }
+}
